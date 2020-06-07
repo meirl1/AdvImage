7d25cc766826b80344092754b8559ac629a7c820
meirl1 <meirl@g.jct.ac.il>
2020-06-07 04:30:17 +0300

    Bug Fixes, meaningful names for variables

--- testIFGS.py
+++ testIFGS.py
@@ -39,7 +39,7 @@ def get_imagenet_label(probs):
 
 # %%
 loss_object = tf.keras.losses.CategoricalCrossentropy()
-
+#FGSM
 def create_adversarial_pattern(input_image, input_label):
   with tf.GradientTape() as tape:
     tape.watch(input_image)
@@ -64,60 +64,59 @@ def display_images(image, description):
 
 
 # %%
-#Iterative Fast Gradient Sign
+#Iterative Fast Gradient Sign method
 def IFGS(image,d_class,eps = 0.001):
   print('Target deceiving class: {}'.format(np.argmax(d_class)))
   perturbations = create_adversarial_pattern(image,d_class)
   adv_x = image - eps*perturbations
-  adv_x = tf.clip_by_value(adv_x,0,1)
+  adv_x = tf.clip_by_value(adv_x,-1,1)
   adv_probs = pretrained_model.predict(adv_x)
-  i=0
+  iterCount=1
   while np.argmax(adv_probs) != np.argmax(d_class):#Keep looping until we get the aversarial label
     perturbations += create_adversarial_pattern(adv_x,d_class)
     adv_x = image - eps*perturbations
-    adv_x = tf.clip_by_value(adv_x,0,1)
+    adv_x = tf.clip_by_value(adv_x,-1,1)
     adv_probs = pretrained_model.predict(adv_x)
-    print('Attempt: {} image label: {}, Index: {}'.format(i,get_imagenet_label(adv_probs)[1],np.argmax(adv_probs)))
+    print('Attempt: {} image label: {}, Index: {}'.format(iterCount,get_imagenet_label(adv_probs)[1],np.argmax(adv_probs)))
     #displays progress
-    i+=1
-  return perturbations ,i
+    iterCount+=1
+  return perturbations ,iterCount
 
 
 # %%
-def IAN_generation(image,d_class,eps=0.001,increment=0.001):
+def IAN_generation(image,d_class,eps=0.005,increment=0.005):
     image_probs = pretrained_model.predict(image)
-    perturbations, i = IFGS(image,d_class,eps)
+    perturbations, iterCount = IFGS(image,d_class,eps)
     adv_x = image - eps*perturbations
-    adv_x = tf.clip_by_value(adv_x,0,1)
-    adv_probs = pretrained_model.predict(adv_x)
+    adv_x = tf.clip_by_value(adv_x,-1,1)
+    #adv_probs = pretrained_model.predict(adv_x)
     filtered_x = tf_filters.median_filter2d(adv_x,(5,5))
     filtered_probs = pretrained_model.predict(filtered_x)
     while np.argmax(filtered_probs) == np.argmax(image_probs):
         eps += increment
         perturbations, j = IFGS(image,d_class,eps)
-        i += j
+        iterCount += j
         adv_x = image - eps*perturbations
-        adv_x = tf.clip_by_value(adv_x,0,1)
-        adv_probs = pretrained_model.predict(adv_x)
+        adv_x = tf.clip_by_value(adv_x,-1,1)
+        #adv_probs = pretrained_model.predict(adv_x)
         filtered_x = tf_filters.median_filter2d(adv_x,(5,5))
         filtered_probs = pretrained_model.predict(filtered_x)
-        print('epsilon: {}, Filtered image label: {} Index: {}'.format(eps,get_imagenet_label(filtered_probs)[1],np.argmax(filtered_probs)))
-    return eps*perturbations, eps, i
-
+        print('eps: {},iterCount: {}, Filtered image label: {} Index: {}'.format(eps,iterCount,get_imagenet_label(filtered_probs)[1],np.argmax(filtered_probs)))
+    return eps*perturbations, eps, iterCount
 
 # %%
 #example 
 
-root_path = 'Enter here imagenet 2012 validation path \\ILSVRC2012_val_0000'
+root_path = 'imagenet2012 validation folder\\ILSVRC2012_val_00000'
 
 adv_success=0
 filter_success=0
 resHeaders = ['Original','Target','Actual','Filtered','FGS Iter','eps']
 results = []
 random.seed(0)
-for i in range(1,10): #Image ILSVRC2012_val_00000003.jpeg is the same image used on the research paper
-    print('file:'+root_path+f'{i:03}.JPEG')
-    image_raw = tf.io.read_file(root_path+f'{i:03}.JPEG')#You may change it to any file you like
+for imgNumber in range(3,4): #Image ILSVRC2012_val_00000003.jpeg is the same image used on the research paper
+    print('file:'+root_path+f'{imgNumber:03}.JPEG')
+    image_raw = tf.io.read_file(root_path+f'{imgNumber:03}.JPEG')#You may change it to any file you like
     image = tf.image.decode_image(image_raw)
     if image.shape[2] == 1:
         image = tf.image.grayscale_to_rgb(image)
@@ -126,18 +125,20 @@ for i in range(1,10): #Image ILSVRC2012_val_00000003.jpeg is the same image used
     label = tf.one_hot(np.argmax(image_probs), image_probs.shape[-1])
     label = tf.reshape(label,(1,image_probs.shape[-1]))
     print('Image label: {}, Index: {}'.format(get_imagenet_label(image_probs)[1],np.argmax(image_probs)))
-    #display_images(image,'input')
-    randnum = random.randint(1,1000)
-    d_class = tf.one_hot(randnum,1000)
+    display_images(image,'input')
+    
+    dNumber = random.randint(1,1000)
+    #for dNumber in range(1,1000):
+    #if dNumber != 230:
+    d_class = tf.one_hot(dNumber,1000)
     d_class = tf.reshape(d_class,(1,1000))
-    delta, eps, i = IAN_generation(image,d_class)
+    delta, eps, iterCount = IAN_generation(image,d_class)
     adv_x = image - delta
-    adv_x = tf.clip_by_value(adv_x,0,1)
+    adv_x = tf.clip_by_value(adv_x,-1,1)
     adv_probs = pretrained_model.predict(adv_x)
     print('Adv image label: {}, Index: {}'.format(get_imagenet_label(adv_probs)[1],np.argmax(adv_probs)))
-    
-    #display_images(adv_x,'input')
-
+    #results.append((np.argmax(image_probs),dNumber,np.argmax(adv_probs),np.argmax(filtered_probs),iterCount,eps))
+    display_images(adv_x,'input')
     if np.argmax(adv_probs) != np.argmax(image_probs):
         print('Success')
         adv_success+=1
@@ -147,25 +148,9 @@ for i in range(1,10): #Image ILSVRC2012_val_00000003.jpeg is the same image used
     filtered_x = tf_filters.median_filter2d(adv_x,(5,5))
     filtered_probs = pretrained_model.predict(filtered_x)
     print('Filtered image label: {} Index: {}'.format(get_imagenet_label(filtered_probs)[1],np.argmax(filtered_probs)))
-    #display_images(filtered_x,'input')
+    display_images(filtered_x,'input')
 
     if np.argmax(filtered_probs) == np.argmax(image_probs):
         filter_success+=1
-    results.append((np.argmax(image_probs),randnum,np.argmax(adv_probs),np.argmax(filtered_probs),i,eps))
+    results.append((np.argmax(image_probs),dNumber,np.argmax(adv_probs),np.argmax(filtered_probs),iterCount,eps))
 print('{} {}'.format(adv_success,filter_success))
-
-#For any explanation please contact me
-
-
-# %%
-results
-
-
-# %%
-#decode_predictions(np.zeros((1,1000)),top=1000)[0][1]
-decode_predictions(d_class.numpy())
-
-
-# %%
-results
-
