3f554d0b095c1cbf89f8df4c12bdf07df4fd4193
meirl1 <meirl@g.jct.ac.il>
2020-08-10 12:13:32 +0300

    updated kmeans

--- ExpLossDiff.py
+++ ExpLossDiff.py
@@ -99,7 +99,7 @@ data = list_ds.map(parse_image)
 
 
 def IFGS(image, d_class, eps=0.001):
-    print('Target deceiving class: {}'.format(np.argmax(d_class)))
+    #print('Target deceiving class: {}'.format(np.argmax(d_class)))
     perturbations = create_adversarial_pattern(image, d_class)
     adv_x = image - eps*perturbations
     adv_x = tf.clip_by_value(adv_x, -1, 1)
@@ -111,8 +111,8 @@ def IFGS(image, d_class, eps=0.001):
         adv_x = image - eps*perturbations
         adv_x = tf.clip_by_value(adv_x, -1, 1)
         adv_probs = pretrained_model.predict(adv_x)
-        print('Attempt: {} image label: {}, Index: {}'.format(
-            iterCount, decode_predictions(adv_probs, top=1)[0][0][1], np.argmax(adv_probs)))
+        '''print('Attempt: {} image label: {}, Index: {}'.format(
+            iterCount, decode_predictions(adv_probs, top=1)[0][0][1], np.argmax(adv_probs)))'''
         # displays progress
         iterCount += 1
     return perturbations, iterCount
@@ -139,14 +139,13 @@ def IAN_generation(image, d_class, eps=0.005, increment=0.005):
             eps, iterCount, decode_predictions(filtered_probs, top=1)[0][0][1], np.argmax(filtered_probs)))
     return eps*perturbations, eps, iterCount
 # %%
-
+#prepare clusters
 
 n = 99
 k = 3
 # k Number of clusters
 # n Number of training data
 # c Number of features in the data
-c = data.__iter__().next()[0].shape
 # Generate random centers, here we use sigma and mean to ensure it represent the whole data
 m = tf.keras.metrics.MeanTensor()
 for img in data:
@@ -163,8 +162,7 @@ print('std: {}'.format(std))
 centers = []
 centers_old = []
 for i in range(k):
-    centers.append(tf.random.normal(
-        data.__iter__().next()[0].shape, mean, std, seed=0))
+    centers.append(tf.random.normal(data.__iter__().next()[0].shape, mean, std, seed=0))
     centers_old.append(tf.zeros(data.__iter__().next()[0].shape))
 # centers_old = tf.zeros(centers.shape) # to store old centers
 centers_new = deepcopy(centers)  # Store new centers
@@ -183,7 +181,7 @@ while error > 0.0001:
     for i in range(k):
         j = 0
         for img in data:
-            distances[j, i] = tf.linalg.norm(img[0] - centers[i])
+            distances[j, i] = tf.linalg.norm(img[0] - centers_new[i])
             j += 1
     # Assign all training data to closest center
     clusters = np.argmin(distances, axis=1)
@@ -204,6 +202,7 @@ while error > 0.0001:
         error += tf.linalg.norm(centers_new[i] - centers_old[i])
     error /= k
     print('error: {}'.format(error))
+np.savetxt('clusters.csv',clusters,delimiter=',')
 print(centers_new)
 #%%
 import pandas as pd
@@ -216,7 +215,7 @@ for img in mn_data:
     idx_list.append(np.argmax(pretrained_model.predict(img[0])))
 
 df = pd.DataFrame(index=file_list,columns=idx_list)
-df
+df = df.loc[:,~df.columns.duplicated()]
 # %%
 files_1 = []
 files_2 = []
@@ -230,25 +229,24 @@ for filename in list_ds:
     else:
         files_3.append(filename)
     i += 1
-
 # %%
 adv_success = 0
 filter_success = 0
 results = []
 row = 0
-for img in mn_data:
+for img in mn_data.shuffle(100).take(10):
     image = img[0]
     image_probs = pretrained_model.predict(image)
     label = tf.one_hot(np.argmax(image_probs), image_probs.shape[-1])
     label = tf.reshape(label, (1, image_probs.shape[-1]))
     display_images(image, 'input')
     column = 0
-    for dNumber in idx_list:
+    for dNumber in df.columns:
         if np.argmax(image_probs) != dNumber:
             d_class = tf.one_hot(dNumber, 1000)
             d_class = tf.reshape(d_class, (1, 1000))
+            print('decieving class: ',dNumber)
             delta, eps, df.iloc[row,column] = IAN_generation(image, d_class)
-            print(df.iloc[row,column])
             adv_x = image - delta
             adv_x = tf.clip_by_value(adv_x, -1, 1)
             adv_probs = pretrained_model.predict(adv_x)
@@ -260,14 +258,22 @@ for img in mn_data:
             else:
                 print('Failed')
         column += 1
-
+    print('label: {}\nmean: {}'.format(np.argmax(image_probs),df.mean(axis=1)))
+    for i in range(k):
+        print('cluster {}\n\
+            mean: {}'.format(np.array(idx_list)[clusters == i],df[np.array(idx_list)[clusters == i]].mean(axis=1)))
     filtered_x = filters.median_filter2d(adv_x, (5, 5))
     filtered_probs = pretrained_model.predict(filtered_x)
     #display_images(filtered_x, 'input')
 
     if np.argmax(filtered_probs) == np.argmax(image_probs):
         filter_success += 1
+    row += 1
 print('{} {}'.format(adv_success, filter_success))
+df.to_csv('Kmeanscenters.csv')
 
+# %%
+print(clusters)
+#check second high value in prediction results
 
 # %%
